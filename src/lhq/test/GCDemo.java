a8f5cd633277ab57a21c31e015635a8ce8a74496
lhq <2444011501@qq.com>
2021-08-19 10:25:30 +0800

    second commit

--- src/lhq/test/GCDemo.java
+++ src/lhq/test/GCDemo.java
@@ -1,9 +1,10 @@
 package lhq.test;
 
 public class GCDemo {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         System.out.println(Integer.MAX_VALUE/1000/60/60/24);
         System.out.println("GCDemo......");
         //Thread.sleep(Integer.MAX_VALUE);
+        //test
     }
 }
